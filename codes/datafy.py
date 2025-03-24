b3a87d892165fa024be5236b185dcfdb25849a90
sermin asusi7 <sermin@asusi7.machine>
2025-03-24 01:17:56 +0100

    more

--- codes/datafy.py
+++ codes/datafy.py
@@ -4,18 +4,74 @@ from pathlib import Path
 import yaml
 
 
+class DoubleDumper(yaml.Dumper):
+    def represent_scalar(self, tag, value, style=None):
+        if tag == 'tag:yaml.org,2002:str':
+            style = '"'
+        return super().represent_scalar(tag, value, style=style)
+
+
+class Crewie:
+    def __init__(self, name, role) -> None:
+        super().__init__()
+        self.name = name
+        self.role = role
+
+    @property
+    def data(self) -> dict:
+        data = {self.name: self.role}
+        return data
+
+    @classmethod
+    def create_from_dict_v1(cls, indx, crew_dict) -> Crewie:
+        pass
+        name, role = list(crew_dict.items())[0]
+        crew = cls(name, role)
+        return crew
+        # name = crew_dict['name']
+        # role = crew_dict['role']
+
+
+#     # print(f'Crew.create_from_dict_v1: {crew_dict}')
+#     key = list(crew_dict.keys())[0]
+#     val = crew_dict[key]
+#     name = val['name']
+#     role = val['role']
+#     notes = val.get('notes', '')
+#     crew = cls(indx, name, role, notes)
+#     return crew
+#
+# def print_self(self):
+#     print(f'  -- {self.num} {self.name} ({self.role})')
+#     if self.notes:
+#         print(f'\t{self.notes}')
+
+
 class Track:
-    def __init__(self, num, vinyl, greenlandic, danish, notes='') -> None:
+    def __init__(self, num, vinyl, duration, greenlandic, danish, notes='') -> None:
         super().__init__()
         self.num = num
         self.vinyl = vinyl
+        self.duration = duration
         self.greenlandic = greenlandic
         self.danish = danish
         self.notes = notes
 
+    @property
+    def data(self) -> dict:
+        data = {}
+        # data['num'] = self.num
+        data['vinyl'] = self.vinyl
+        data['duration'] = self.duration
+        data['title'] = {}
+        data['title']['greenlandic'] = self.greenlandic
+        data['title']['danish'] = self.danish
+        data['notes'] = self.notes
+        return data
+
     @classmethod
-    def from_dict_v1(cls, indx, track_dict) -> Track:
-        # print(f'Track.from_dict_v1: {track_dict}')
+    def create_from_dict_v1(cls, indx, track_dict) -> Track:
+        # print(f'Track.create_from_dict_v1: {track_dict}')
         key = list(track_dict.keys())[0]
         val = track_dict[key]
         vinyl = val['vinyl']
@@ -24,11 +80,13 @@ class Track:
         greenlandic = title_dict['greenlandic']
         danish = title_dict['danish']
         notes = val.get('notes', '')
-        track = cls(indx, vinyl, greenlandic, danish, notes)
+        track = cls(indx, vinyl, duration, greenlandic, danish, notes)
         return track
 
     def print_self(self):
         print(f'  -- {self.num} {self.vinyl}:\n\t{self.greenlandic}\n\t{self.danish}')
+        if self.notes:
+            print(f'\t{self.notes}')
 
 
 class Album:
@@ -42,8 +100,39 @@ class Album:
         self.tracks_list = tracks_list
         self.tracks = []
         for indx, track_dict in enumerate(tracks_list):
-            track = Track.from_dict_v1(indx, track_dict)
+            track = Track.create_from_dict_v1(indx, track_dict)
             self.tracks.append(track)
+        self.crew = []
+        for indx, crew_dict in enumerate(crew_list):
+            crewie = Crewie.create_from_dict_v1(indx, crew_dict)
+            self.crew.append(crewie)
+
+    @property
+    def data(self):
+        data = {}
+        data['title'] = self.title
+        data['year'] = self.year
+        data['description'] = self.description
+        # data['crew'] = self.crew_list
+        tracks: dict = {}
+        data['tracks'] = tracks
+        for track in self.tracks:
+            tracks[track.num] = track.data
+        crews = []
+        data['crew'] = crews
+        for crewie in self.crew:
+            crews.append(crewie.data)
+        return data
+
+        # data['tracks'] = []
+        # for track in self.tracks:
+        #     track_data = {}
+        #     track_data['vinyl'] = track.vinyl
+        #     track_data['title'] = {}
+        #     track_data['title']['greenlandic'] = track.greenlandic
+        #     track_data['title']['danish'] = track.danish
+        #     track_data['notes'] = track.notes
+        #     data['tracks'].append(track_data)
 
     def print_self(self):
         print(f'- Album {self.num}')
@@ -53,8 +142,8 @@ class Album:
             track.print_self()
 
     @classmethod
-    def from_dict_v1(cls, num, album_dict) -> Album:
-        # print(f'Album.from_dict_v1: {album_dict}')
+    def create_from_dict_v1(cls, num, album_dict) -> Album:
+        # print(f'Album.create_from_dict_v1: {album_dict}')
         title = album_dict['title']
         year = album_dict['year']
         description = album_dict['description']
@@ -70,9 +159,55 @@ class Albumor:
         print("Albumor is ready.")
         self.data_dir = Path('/opt/projects/liva_sume/data')
         self.data_file = self.data_dir / 'albums.yml'
+        self.data_v2_file = self.data_dir / 'albums.v2.yml'
         self.md_file = self.data_dir / 'sume_albums.md'
+        self.md_parsed_file = self.data_dir / 'sume_albums.parsed.md'
         self.lines_file = self.data_dir / 'lines.yml'
         self.albums: list[Album] = []
+
+    def yaml_dump(self, data_v2):
+        return yaml.dump(data_v2, Dumper=DoubleDumper, sort_keys=False)
+
+    def parse_v1(self):
+        data_txt = self.data_file.read_text()
+        data = yaml.safe_load(data_txt)
+        # data_yml = yaml.dump(data)
+        # print(data_yml)
+        albums_dict = data['albums']
+        for num, album_dict in albums_dict.items():
+            # print(f' - {album_dict["title"]}')
+            album = Album.create_from_dict_v1(num, album_dict)
+            self.albums.append(album)
+            album.print_self()
+
+    def generate_v2(self) -> dict:
+        data: dict = {}
+        albums: dict = {}
+        data['albums'] = albums
+        for album in self.albums:
+            albums[album.num] = album.data
+
+        data_yml = yaml.safe_dump(data, sort_keys=False)
+        print(data_yml)
+        return data
+
+        # data['albums'] = {}
+        # for album in self.albums:
+        #     album_dict = {}
+        #     album_dict['title'] = album.title
+        #     album_dict['year'] = album.year
+        #     album_dict['description'] = album.description
+        #     album_dict['crew'] = album.crew_list
+        #     album_dict['tracks'] = []
+        #     for track in album.tracks:
+        #         track_dict = {}
+        #         track_dict['vinyl'] = track.vinyl
+        #         track_dict['title'] = {}
+        #         track_dict['title']['greenlandic'] = track.greenlandic
+        #         track_dict['title']['danish'] = track.danish
+        #         track_dict['notes'] = track.notes
+        #         album_dict['tracks'].append(track_dict)
+        #     data['albums'][album.num] = album_dict
 
     def test1(self):
         data_txt = self.data_file.read_text()
@@ -114,22 +249,64 @@ class Albumor:
             #     print(f'{track_num}: {greenlandic} - {danish}')
 
     def test4(self):
-        data_txt = self.data_file.read_text()
+        self.parse_v1()
+        print('=====================================')
+        data_v2 = self.generate_v2()
+        # v2_yml = yaml.dump(data_v2, sort_keys=False)
+        v2_yml = self.yaml_dump(data_v2)
+        self.data_v2_file.write_text(v2_yml)
+
+    def test5(self):
+        data_txt = self.data_v2_file.read_text()
         data = yaml.safe_load(data_txt)
         # data_yml = yaml.dump(data)
         # print(data_yml)
+        md_txt = '# Sume Albums\n\n'
         albums_dict = data['albums']
+
+        md_txt += f'## Albums:\n'
         for num, album_dict in albums_dict.items():
-            # print(f' - {album_dict["title"]}')
-            album = Album.from_dict_v1(num, album_dict)
-            self.albums.append(album)
-            album.print_self()
+            title = f'"{album_dict["title"]}"'
+            year = album_dict['year']
+            md_txt += f'{num}. {year},  {title}\n'
+
+        for num, album_dict in albums_dict.items():
+            # print(num, album_dict['title'])
+            title = album_dict['title']
+            year = album_dict['year']
+            description = album_dict['description']
+            md_txt += f'## Album #{num} "{title}" ({year})\n'
+            md_txt += f'{description}\n\n'
+
+            tracks_list = album_dict['tracks']
+            md_txt += '### Tracks\n'
+            for track_num, track_dict in tracks_list.items():
+                vinyl = track_dict['vinyl']
+                title_dict = track_dict['title']
+                greenlandic = title_dict['greenlandic']
+                danish = title_dict['danish']
+                notes = track_dict.get('notes', '')
+                md_txt += f'{track_num + 1}. {vinyl}: {greenlandic} - ({danish})\n'
+                if notes:
+                    md_txt += f'\t{notes}\n'
+
+            crew_list = album_dict['crew']
+            md_txt += '### Crew\n'
+            for crew_dict in crew_list:
+                for name, role in crew_dict.items():
+                    md_txt += f'- {name} ({role})\n'
+            md_txt += '\n'
+
+            md_txt += '\n'
+
+        print(md_txt)
+        self.md_parsed_file.write_text(md_txt)
 
 
 def main():
     # print("Hello, World!")
     albumor = Albumor()
-    albumor.test4()
+    albumor.test5()
 
 
 if __name__ == "__main__":
---
+++ data/sume_albums.parsed.md
@@ -0,0 +1,106 @@
+# Sume Albums
+
+## Albums:
+1. 1973,  "Sumut"
+2. 1974,  "Sume"
+3. 1977,  "Sume 3"
+## Album #1 "Sumut" (1973)
+Greenlandic rock band founded in 1972 by Malik Høegh and Per Berthelsen (both vocals and guitar) in Sorø, Denmark, where the founding members studied. Their 1973 debut Sumut was the first rock album in the Greenlandic language and breaking the waves for Greenlandic political rock. In 1977 they released the first album by the first Greenlandic label Ulo - and disbanded the same year. Since then the band has frequently rejoined to perform and in 1994 they released a new album, Persersume, the 100th album release on Ulo. October 2014 saw the release of a documentary retracing the history of the band: "Sume - lyden af en revolution".
+
+### Tracks
+1. A1: Pivfît Nutât - (Nye Tider)
+2. A2: Upernâĸ - (Forår)
+3. A3: Pilerineĸ-Tikíneĸ - (Forventning-Ankomst)
+4. A4: Tamorassâriat - (Den Første Bid Af Sælen)
+5. A5: Ilivnut Pulavdlunga - (Kravle Ind I Mit Indre)
+6. A6: Erĸigsineĸ Sapĩnarama - (Uro)
+7. B1: Erĸasûteĸarneĸ - (Bekymring)
+8. B1: Heimaey Erĸaivdlugo - (Ode Til Heimaey)
+9. B3: Imigagssaĸ - (Ildvand)
+10. B4: Ukiaĸ - (Efterår)
+11. B5: Nalunaerasuartaut Toĸuvoĸ - (Telegrafen Er Død)
+### Crew
+- Ole Høst (Alto Saxophone)
+- P.V. Rasmussen (Artwork [Backside])
+- Aron (11) (Artwork [Woodcut Front Cover])
+- Erik Hammeken, Henrik Anthony (tracks: A2, A5) (Bass)
+- Hans Fleischer (Drums)
+- Anders Peter Novrman (Electric Guitar)
+- Freddy Hansson (Engineer)
+- Jørgen Lang (Harmonica [Mundharpe])
+- Mikael Hansen (2) (Layout [Cover])
+- Malik* (Lyrics By)
+- Malik* (tracks: A1 to B1, B4, B5), Per* (tracks: B3), Sume (tracks: B2), Ungak (tracks: B3) (Music By)
+- Thor Backhausen (Organ, Flute)
+- Karsten Sommer (Producer)
+- Kaj D. Holm (Violin)
+- Per Berthelsen (Vocals, Acoustic Guitar, Electric Guitar)
+- Malik Høegh (Vocals, Twelve-String Guitar)
+
+
+## Album #2 "Sume" (1974)
+Recorded At: Rosenberg Studio
+
+### Tracks
+1. A1: Kiisa Puigulertorpagit! - (Omsider kan jeg glemme dig!)
+2. A2: Avijaaja Ila! - (Avijaaja Ila!)
+3. A3: Aasarisseruttoraa - (Højsommer)
+4. A4: Qullissat - (Qullissat)
+5. B1: Takornartaq - (Den Fremmede)
+6. B2: Naluvara Ilisimaneraa - (Måske Ved Hun Det Ikke)
+7. B3: Nukappiaraq - (Drengen)
+8. B4: Inuit Nunaat - (Menneskenes Lande)
+9. B5: Sulutit - (Dine Vinger)
+### Crew
+- Emil Larsen (Bass, Vocals)
+- Grønlænderkoret Mik* (tracks: B5) (Choir)
+- Aka Høegh (Cover)
+- Hjalmar Dahl (Drums, Percussion)
+- Freddy Hansson (Engineer)
+- Malik* (Lyrics By, Music By)
+- Erik Hammeken (tracks: A2), Per Berthelsen (Music By)
+- Avât-Johan Kleist (tracks: B5) (Organ, Piano, Arranged By [Choir])
+- Susanne Mertz (Photography By)
+- Karsten Sommer (Producer)
+- Franz Beckerlee (tracks: A3) (Synthesizer [Moog])
+- Per Berthelsen (Vocals, Acoustic Guitar)
+- Malik Høegh (Vocals, Twelve-String Guitar)
+
+
+## Album #3 "Sume 3" (1977)
+Recorded At: Sweet Silence Studios
+
+### Tracks
+1. A1: Púke - (Puk)
+	Written-By Kâle*
+2. A2: Ikíngutingâ - (Min Ven (Der Er Ikke Længere Brug For Dig))
+	Written-By Kâle*
+3. A3: Inûneĸ - (Livet)
+	Written-By Kâle*
+4. A4: Sume - (Hvor?)
+	Written-By Mailk*, Pêre*
+5. A5: Sisimiut - (Sisimiut)
+	Written-By Kâle*
+6. B1: Kalâliuvunga - (Jeg - Grønlænder)
+	Written-By Egon Sikivat, Sume
+7. B2: K'augdloringme Igdlinersiorata - (Nye Spor I Det Lyse)
+	Written-By Malik*, Pêre*
+8. B3: K'anoĸ Inûguvit - (Hvordan du end lever)
+	Written-By Kâle*, Malik*
+9. B4: Sikorngâ - (Drivis)
+	Written-By Arĸaluk Høegh Bendtsen*, Malik*, Pêre*
+10. B5: Nunaĸarfît Náparĸigdlugit - (Genrejse)
+	Written-By Kâle*, Malik*
+### Crew
+- Malik Høegh (12-String Acoustic Guitar, Electric Guitar, Vocals)
+- Karl Sivertsen, Per Berthelsen (Acoustic Guitar, Electric Guitar, Vocals)
+- Niels Davidsen (Artwork [Front Drawing])
+- Emil Larsen (Bass, Vocals)
+- Hans Fleischer (Drums)
+- Stig Kreutzfeldt (Engineer)
+- Jørgen Sparre (Layout)
+- Nikolaj Steenstrup (Mellotron, Percussion)
+- Sakio Nielsen (Organ, Grand Piano)
+- Karsten Sommer (Producer)
+
+
